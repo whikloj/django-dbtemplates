09784e60e7909188c0ac0e6f77782e9d487f00ff
Daniel Lovegrove <Daniel.Lovegrove@umanitoba.ca>
2022-04-12 15:17:43 -0500

    Load admin.css in admin

--- dbtemplates/admin.py
+++ dbtemplates/admin.py
@@ -92,7 +92,7 @@ class TemplateAdminForm(forms.ModelForm):
     description = forms.CharField(
         widget=forms.Textarea(attrs={
             'rows': '3',
-            'class': 'vLargeTextField-width-fix',
+            'class': 'vLargeTextField vLargeTextField-width-fix',
         }),
         required=False)
 
@@ -103,6 +103,15 @@ class TemplateAdminForm(forms.ModelForm):
 
 
 class TemplateAdmin(TemplateModelAdmin):
+    class Media:
+        css = {
+            'all': [
+                '%(media_prefix)scss/admin.css'.format(
+                    media_prefix=settings.DBTEMPLATES_MEDIA_PREFIX,
+                ),
+            ]
+        }
+
     form = TemplateAdminForm
     readonly_fields = ['creation_date', 'last_changed']
     fieldsets = (
